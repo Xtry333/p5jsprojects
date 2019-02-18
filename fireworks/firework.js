d0dbdb23e0eee545ebaad9749b5f0516f61c5ab3
Xtry333 <xtry333@gmail.com>
2019-02-18 21:17:20 +0100

    fireworks(with color!) and sparkles

--- fireworks/firework.js
+++ fireworks/firework.js
@@ -22,6 +22,7 @@ class Particle extends Body {
     constructor(position, velocity, friction) {
         super(position, velocity, friction);
         this.lifespan = 255;
+        this.color = color(255, 255, 255);
     }
 
     update() {
@@ -30,7 +31,9 @@ class Particle extends Body {
     }
 
     display() {
-        stroke(this.lifespan);
+        //colorMode(HSB);
+        this.color.setAlpha(this.lifespan);
+        stroke(this.color);
         strokeWeight(4);
         point(this.pos.x, this.pos.y);
     }
@@ -41,11 +44,14 @@ class Firework extends Body {
         super(position, velocity, friction);
         this.particles = [];
         this.exploded = false;
+        this.color = color(random(50, 255), random(50, 255), random(50, 255));
+        this.velLimit = random(-2, 8);
+        this.power = random(2, 4) + this.velLimit / 4;
     }
 
     update() {
         super.update();
-        if (this.vel.y >= 0) {
+        if (this.vel.y >= -this.velLimit) {
             this.explode();
         }
         for (let i = 0; i < this.particles.length; i++) {
@@ -55,10 +61,16 @@ class Firework extends Body {
     }
 
     display() {
+        //colorMode(RGB);
         if (!this.exploded) {
-            stroke(255);
+            stroke(this.color);
             strokeWeight(2);
-            line(this.pos.x, this.pos.y - 2, this.pos.x, this.pos.y + 2);
+            push();
+            translate(this.pos.x, this.pos.y);
+            rotate(this.vel.heading() + HALF_PI);
+            //line(this.pos.x, this.pos.y - 2, this.pos.x, this.pos.y + 2);
+            line(0, 0 - 2, 0, 0 + 2);
+            pop();
         }
         for (let i = 0; i < this.particles.length; i++) {
             this.particles[i].display();
@@ -69,7 +81,10 @@ class Firework extends Body {
         if (!this.exploded) {
             this.exploded = true;
             for (let i = 0; i < 50; i++) {
-                this.particles.push(new Particle(this.pos, p5.Vector.random2D().mult(5), 0.5 * random(2)));
+                let p = new Particle(this.pos, p5.Vector.random2D().mult(this.power), 0.5 * random(2));
+                p.applyForce(this.vel);
+                p.color = this.color;
+                this.particles.push(p);
             }
             ellipse(this.pos.x, this.pos.y, 30, 30);
         }
--- fireworks/index.js
+++ fireworks/index.js
@@ -1,10 +1,15 @@
 let firework = [];
+let particles = [];
 let gravity;
+let sparkles = 5;
 
 function setup() {
-    createCanvas(1600, 800);
+    var cnv = createCanvas(windowWidth, windowHeight);
+    cnv.style('display', 'block');
+    let body = document.getElementsByTagName('body')[0];
+    body.style.margin = 0;
     background(0);
-    gravity = createVector(0, 0.25);
+    gravity = createVector(0, 0.1);
     for (let i = 0; i < 20; i++) {
         firework.push(new Firework(createVector(random(width), -50), createVector(0, random(-15, -5)), 0.1));
     }
@@ -15,7 +20,7 @@ function draw() {
 
     for (let i = 0; i < firework.length; i++) {
         if (firework[i].pos.y > height) {
-            firework[i] = new Firework(createVector(random(width), height), createVector(0, random(-23, -10)), 0.1);
+            firework[i] = new Firework(createVector(random(width), height), createVector(random(-2, 2), random(-20, -5)), 0.1);
         }
 
         firework[i].applyForce(gravity);
@@ -23,6 +28,24 @@ function draw() {
         firework[i].display();
     }
 
-    stroke(255);
-    ellipse(mouseX, mouseY, 15, 15);
+    for (let i = particles.length - 1; i >= 0; i--) {
+        if (particles[i].pos.y > height) {
+            //particles = particles.filter();
+        }
+        particles[i].applyForce(gravity);
+        particles[i].update();
+        particles[i].display();
+    }
+
+    if (mouseIsPressed) {
+        for (let i = 0; i < sparkles; i++) {
+            let pos = createVector(mouseX, mouseY);
+            let vel = p5.Vector.random2D().mult(random(8));
+            let particle = new Particle(pos, vel, random(0, 0.1));
+            particle.color = color(random(190, 255), random(90, 255), 0);
+            particles.push(particle);
+        }
+    }
+    //stroke(255);
+    //ellipse(mouseX, mouseY, 15, 15);
 }
