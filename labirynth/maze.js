25b8c9133af15006a03072ccdae74dfd9fd95325
Xtry333 <xtry333@gmail.com>
2019-03-21 16:51:09 +0100

    attempts

--- labirynth/maze.js
+++ labirynth/maze.js
@@ -3,7 +3,7 @@ class Labirynth {
         this.width = width;
         this.height = height;
         this.data = Labirynth.empty(width, height);
-        this.wallSize = 20;
+        this.wallSize = 25;
     };
 
     get(x, y) {
@@ -11,20 +11,24 @@ class Labirynth {
     }
 
     getAdjacent(x, y) {
+        if (x._class === "Cell") {
+            return this.getAdjacent(x.x, x.y);
+        }
         const adjacent = [];
+
         if (x > 0) {
             adjacent.push(this.get(x - 1, y));
         }
         if (y > 0) {
             adjacent.push(this.get(x, y - 1));
         }
-        if (x < width - 1) {
+        if (x < this.width - 1) {
             adjacent.push(this.get(x + 1, y));
         }
-        if (y < height - 1) {
+        if (y < this.height - 1) {
             adjacent.push(this.get(x, y + 1));
         }
-        return adjacent;
+        return adjacent.filter(x => x.value == 0);
     }
 
     static empty(width, height, val) {
@@ -40,6 +44,36 @@ class Labirynth {
     };
 }
 
+class AStar {
+    static pathfind(lab, start, target) {
+        if (target.value == 1) {
+            return "Target unreachable";
+        }
+        const hq = [];
+        const previousCells = [];
+        heapq.push(hq, [0, start, 0]);
+        let iterations = 0;
+        while (hq.length > 0 && iterations++ < 50) {
+            console.log(iterations);
+            const current = heapq.pop(hq);
+            previousCells.push(current);
+            console.log(current);
+            fill(255,0,0);
+            rect(5 + current[1].x * lab.wallSize, 5 + current[1].y * lab.wallSize, lab.wallSize, lab.wallSize);
+
+            if (current[1].equals(target)) {
+                console.log(previousCells);
+                return current;
+            }
+            for (const adj of lab.getAdjacent(current[1])) {
+                if (!previousCells.includes(adj)) {
+                    heapq.push(hq, [current[2] + current[1].dist(target), adj, current[2] + 1]);
+                }
+            }
+        }
+    }
+}
+
 class Cell {
     constructor(x, y) {
         this._class = "Cell";
@@ -47,10 +81,22 @@ class Cell {
         this._y = y;
         this.value = 0;
     }
+
     get x() {
-        return _x;
+        return this._x;
     }
+
     get y() {
-        return _y;
+        return this._y;
+    }
+
+    equals(target) {
+        return this.x == target.x && this.y == target.y;
+    }
+
+    dist(target) {
+        const dx = this.x - target.x;
+        const dy = this.y - target.y;
+        return target._class === "Cell" && Math.sqrt(dx * dx + dy * dy);
     }
 }
--- labirynth/sketch.js
+++ labirynth/sketch.js
@@ -1,46 +1,57 @@
-const comparer = (x, y) =>  x[0] < y[0];
-
-const hq = []
-heapq.push(hq, [4, 'a'])
-heapq.push(hq, [1, 'b'])
-heapq.push(hq, [3, 'c'])
-heapq.push(hq, [0, 'd'])
-heapq.push(hq, [0, 'n'])
-heapq.push(hq, [4, 'f'])
-heapq.push(hq, [4, 'd'])
-heapq.push(hq, [8, 'l'])
-
-console.log(hq);
-console.log(heapq.pop(hq));
-console.log(heapq.pop(hq));
-console.log(hq);
-
-
-const WALL_SIZE = 20;
-
-const walls = [];
-
-
-let lab = new Labirynth(30, 30)
+let lab = new Labirynth(10, 5);
+lab.get(1,1).value = 1;
+lab.get(2,2).value = 1;
+lab.get(3,2).value = 1;
+lab.get(3,0).value = 1;
+lab.get(3,3).value = 1;
 
 function setup() {
     var cnv = createCanvas(windowWidth, windowHeight);
     
     cnv.style('display', 'block');
     background(255);
-}
 
-function draw() {
-    background(255);
+    frameRate(2);
 
-    fill(230);
     for (let y = 0; y < lab.height; y++) {
         for (let x = 0; x < lab.width; x++) {
-            if (lab.get(x, y).value == 0) {
+            const cell = lab.get(x, y)
+            if (cell.value == 0) {
+                fill(240);
                 rect(5 + x * lab.wallSize, 5 + y * lab.wallSize, lab.wallSize, lab.wallSize);
-                //fill(127);
-                //rect(5 + x * lab.wallSize+1, 5 + y * lab.wallSize+1, lab.wallSize-2, lab.wallSize-2);
+            } else {
+                fill(33);
+                rect(5 + x * lab.wallSize, 5 + y * lab.wallSize, lab.wallSize, lab.wallSize);
             }
+            //if (cell.)
         }
     }
+}
+
+let start = lab.get(0,0);
+let target = lab.get(9,4);
+const hq = [];
+const previousCells = [];
+heapq.push(hq, [0, start, 0]);
+let iterations = 0;
+
+function draw() {
+    //background(255); 
+    console.log(iterations);
+    const current = heapq.pop(hq);
+    previousCells.push(current);
+    console.log(current);
+    fill(255,0,0);
+    rect(5 + current[1].x * lab.wallSize, 5 + current[1].y * lab.wallSize, lab.wallSize, lab.wallSize);
+
+    if (current[1].equals(target)) {
+        console.log(previousCells);
+        return current;
+    }
+    for (const adj of lab.getAdjacent(current[1])) {
+        if (!previousCells.includes(adj)) {
+            heapq.push(hq, [current[2] + current[1].dist(target), adj, current[2] + 1]);
+        }
+    }
+    
 }
