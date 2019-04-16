9fa2ccbfe053e5863d660545811c196558fc3e9d
Xtry333 <xtry333@gmail.com>
2019-04-17 00:46:47 +0200

    wasd movement

--- view/sketch.js
+++ view/sketch.js
@@ -1,12 +1,43 @@
+let viewX = 0
+let viewY = 0
+let pos = null
+let viewOffset = null
+
 function setup() {
     var cnv = createCanvas(windowWidth, windowHeight)
 
     cnv.style('display', 'block')
     background(255)
+    pos = createVector(0, 0)
 }
 
 function draw() {
     background(255)
     fill(0)
+
+    viewOffset = createVector(0, 0)
+    if (keyIsDown(65)) {
+        viewOffset.x = -1
+    }
+    if (keyIsDown(68)) {
+        viewOffset.x = 1
+    }
+    if (keyIsDown(87)) {
+        viewOffset.y = -1
+    }
+    if (keyIsDown(83)) {
+        viewOffset.y = 1
+    }
+    pos.add(viewOffset.normalize())
+
+    rect(Math.round(pos.x), Math.round(pos.y), 50, 50)
     ellipse(mouseX, mouseY, 15, 15)
 }
+
+// function keyPressed() {
+//     if (key == 'a') {
+//         viewOffset.x--
+//     } else if (key == 'd') {
+//         viewOffset.x++
+//     }
+// }
